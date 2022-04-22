ebdd2c10f98bfd8a3b1b1ef00bf55efea594f3f6
Lucas <lucas@squadeasy.com>
2022-04-22 11:33:06 +0200

    docker test

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express  from "express";
+import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.send('Hello world:');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log('Listening on' + port);
+  console.log(`Example app listening on port ${port}`);
 });
