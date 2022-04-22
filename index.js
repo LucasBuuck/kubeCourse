a46b3ebe2f7f1abf5f8d70649c7a69bc710a2119
Lucas <lucas@squadeasy.com>
2022-04-22 13:56:48 +0200

    Update again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Updated !");
+  res.send("Hello World! Updated ! Updated again");
 });
 
 app.listen(port, () => {
